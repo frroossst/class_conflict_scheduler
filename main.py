0c44fba6f430fb6ce34d1779f456053c6cd1cc74
Adhyan <adhyanpatel@gmail.com>
2024-11-10 19:03:21 -0600

    added cli args parser

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from parser import Parser
 from scheduler import Scheduler
+from parser import Parser
 
+import argparse
+import os
 
-p = Parser("schedule.toml")
-c = p.parse()
 
 YELLOW = "\033[33m"
 CYAN = "\033[36m"
@@ -11,6 +11,20 @@ BOLD = "\033[1m\033[37m"
 RED = "\033[31m"
 GREEN = "\033[32m"
 RESET = "\033[0m"
+
+desc = "select the best class according to your schedule"
+clap = argparse.ArgumentParser(description=desc)
+clap.add_argument("file", type=str, help="path to the schedule TOML file")
+args = clap.parse_args()
+
+path = args.file
+if not os.path.exists(path):
+    raise RuntimeError(f"{BOLD}{RED}[ERROR]{RESET} {path}: no such file exists")
+elif not path.endswith(".toml"):
+    raise RuntimeError(f"{BOLD}{RED}[ERROR]{RESET} {path}: must be a TOML file")
+
+p = Parser(path)
+c = p.parse()
 
 print(f"{BOLD}============== [SCHEDULER] =============={RESET}")
 print(f"{BOLD}{GREEN}[TAKEN]{RESET}")
@@ -24,3 +38,4 @@ print(f"{BOLD}========================================={RESET}")
 
 s = Scheduler(c["taken_classes"], c["wanted_classes"])
 s.report()
+
